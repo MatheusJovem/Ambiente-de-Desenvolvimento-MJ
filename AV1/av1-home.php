baf2b3ea37c4ae135caaff120cbb49be974b1442
MatheusJovem <90429176+MatheusJovem@users.noreply.github.com>
2021-11-11 08:28:51 -0300

    Update av1-home.php

--- AV1/av1-home.php
+++ AV1/av1-home.php
@@ -5,14 +5,13 @@
 		<h2>AV1 - Matheus Jovem Seixas - 2010478300050</h2>
 	</head>
 	<body>
-			<h1>Home</h1>
-			<a href="av1_CriarPergunta.php">Criar Pergunta</a><br>
-			<a href="av1_AlterarPergunta.php">Alterar Pergunta</a><br>
-			<a href="av1_ListarTodasPerguntas.php">Listar todas Perguntas</a><br>
-			<a href="av1_ListarUmaPergunta.php">Listar uma Pergunta</a><br>
-			<a href="av1_ExcluirPergunta.php">Excluir Pergunta</a><br>
-			<br>
-		
+		<h1>Home</h1>
+		<a href="av1_CriarPergunta.php">Criar Pergunta</a><br>
+		<a href="av1_AlterarPergunta.php">Alterar Pergunta</a><br>
+		<a href="av1_ListarTodasPerguntas.php">Listar todas Perguntas</a><br>
+		<a href="av1_ListarUmaPergunta.php">Listar uma Pergunta</a><br>
+		<a href="av1_ExcluirPergunta.php">Excluir Pergunta</a><br>
+		<br>
 	</body>
 </html>
 <?php

c536d002726c08aa112fbc9ee41daeaf1c634607
MatheusJovem <90429176+MatheusJovem@users.noreply.github.com>
2022-03-10 09:12:57 -0300

    Add files via upload

---
+++ 3DAW 2022.1/ex02.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>3DAW - Exercício 02 - Matheus Jovem Seixas - 2010478300050</title>
+    </head>
+    <body>
+        <?php
+            echo "<h1>3DAW</h1>";
+            echo "<h2>Exercício 02</h2>";
+            // comentário simples
+
+            $var = "string";
+            echo $var;
+
+            $var = 10 + 5;
+            echo $var;
+        ?>
+    </body>
+</html>

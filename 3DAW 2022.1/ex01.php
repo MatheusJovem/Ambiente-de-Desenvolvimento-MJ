f13312e71935e61ac0f61a23c5b22cf686d71fe0
MatheusJovem <90429176+MatheusJovem@users.noreply.github.com>
2022-03-10 08:48:44 -0300

    Add files via upload

---
+++ 3DAW 2022.1/ex01.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>3DAW - Exercício 01 - Matheus Jovem Seixas - 2010478300050</title>
+    </head>
+    <body>
+        <?php
+            echo "<h1>Exercício 01</h1>";
+            echo "<p>Olá Mundo</p>"
+        ?>
+    </body>
+</html>

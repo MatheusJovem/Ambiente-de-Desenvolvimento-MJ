fc18bbedda8f86abe4d10f5acbb5ac435f8a01ae
MatheusJovem <90429176+MatheusJovem@users.noreply.github.com>
2022-03-10 09:03:40 -0300

    Update ex01.php

--- 3DAW 2022.1/ex01.php
+++ 3DAW 2022.1/ex01.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
     <head>
         <meta charset="UTF-8">
         <title>3DAW - Exercício 01 - Matheus Jovem Seixas - 2010478300050</title>
     </head>
     <body>
         <?php
-            echo "<h1>Exercício 01</h1>";
-            echo "<p>Olá Mundo</p>"
+            echo "<h1>3DAW</h1>";
+            echo "<h2>Exercício 01</h2>";
+            echo "<p>Olá Mundo</p>";
         ?>
     </body>
 </html>

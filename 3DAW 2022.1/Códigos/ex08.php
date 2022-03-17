99d9a3d454edaacd8d8426d3225df18e9003de6c
MatheusJovem <90429176+MatheusJovem@users.noreply.github.com>
2022-03-17 10:24:39 -0300

    Add files via upload

---
+++ 3DAW 2022.1/Códigos/ex08.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>3DAW - Exercício 08 - Matheus Jovem Seixas - 2010478300050</title>
+        <style>
+            body{
+                text-align: center;
+                font-family: Verdana;
+            }
+
+        </style>
+    </head>
+    <body>
+        <?php
+            echo "<h1>3DAW</h1>";
+            echo "<h2>Exercício 08 - Numeros</h2>";
+
+            $variavel = 8;
+            echo "<br>";
+
+            var_dump(is_int($variavel));
+            echo "<br>";
+
+            echo $variavel;
+            echo "<br>";
+
+            echo gettype($variavel);
+            echo "<br>";
+        ?>
+    </body>
+</html>

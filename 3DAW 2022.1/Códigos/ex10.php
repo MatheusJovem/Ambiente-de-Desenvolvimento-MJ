db996937f9145b1eadfa932e174d517598b8d7cc
MatheusJovem <90429176+MatheusJovem@users.noreply.github.com>
2022-03-17 10:43:50 -0300

    Add files via upload

---
+++ 3DAW 2022.1/Códigos/ex10.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>3DAW - Exercício 09 - Matheus Jovem Seixas - 2010478300050</title>
+        <style>
+            body{
+                text-align: center;
+                font-family: Verdana;
+            }
+
+        </style>
+    </head>
+    <body>
+        <?php
+            echo "<h1>3DAW</h1>";
+            echo "<h2>Exercício 10 - Operadores</h2>";
+
+            echo "Soma 8 + 8: ";
+            echo 8+8;
+            echo "<br><br>";
+
+            echo "Subtração 8 - 4: ";
+            echo 8-4;
+            echo "<br><br>";
+
+            echo "Multiplicação 8 * 8: ";
+            echo 8*8;
+            echo "<br><br>";
+
+            echo "Divisão 8 / 8: ";
+            echo 8/8;
+            echo "<br><br>";
+
+            echo "Resto da Divisão 8 % 4: ";
+            echo 8%4;
+            echo "<br><br>";
+
+            echo "Exponencial 8**2: ";
+            echo 8**2;
+        ?>
+    </body>
+</html>

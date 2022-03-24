c3d8a918739de518ee5a9ec0dff068200d83d5b3
MatheusJovem <90429176+MatheusJovem@users.noreply.github.com>
2022-03-24 08:17:53 -0300

    Add files via upload

---
+++ 3DAW 2022.1/Códigos/ex11.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>3DAW - Exercício 11 - Matheus Jovem Seixas - 2010478300050</title>
+        <style>
+            body{
+                text-align: center;
+                font-family: Verdana;
+            }
+
+        </style>
+    </head>
+    <body>
+        <?php
+            echo "<h1>3DAW</h1>";
+            echo "<h2>Exercício 11 - Constantes</h2>";
+
+            define("cabecalho", "FAETERJ-RIO");
+
+            echo cabecalho;
+
+        ?>
+    </body>
+</html>

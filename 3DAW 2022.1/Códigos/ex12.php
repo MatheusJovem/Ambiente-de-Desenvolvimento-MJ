8c50db6cc2006b037d4d548064f2dd51a4b80650
MatheusJovem <90429176+MatheusJovem@users.noreply.github.com>
2022-03-24 08:24:17 -0300

    Add files via upload

---
+++ 3DAW 2022.1/Códigos/ex12.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>3DAW - Exercício 12 - Matheus Jovem Seixas - 2010478300050</title>
+        <style>
+            body{
+                text-align: center;
+                font-family: Verdana;
+            }
+
+        </style>
+    </head>
+    <body>
+        <?php
+            echo "<h1>3DAW</h1>";
+            echo "<h2>Exercício 12 - If Else Elseif </h2>";
+
+            $a = 5 + 3;
+            $b = 5 - 2;
+            $c = 5 * 2;
+            $d = 5 / 2;
+            $e = 5 % 2;
+            $f = 5 ** 2;
+
+            if($a > 5){
+                echo "Bom Dia";
+            }else{
+                echo "Boa Tarde";
+            }
+
+            echo "<br><br>";
+
+            echo "a = $a";
+            echo "<br>";
+            echo "b = $b";
+            echo "<br>";
+            echo "c = $c";
+            echo "<br>";
+            echo "d = $d";
+            echo "<br>";
+            echo "e = $e";
+            echo "<br>";
+            echo "f = $f";
+            echo "<br>";
+
+        ?>
+    </body>
+</html>

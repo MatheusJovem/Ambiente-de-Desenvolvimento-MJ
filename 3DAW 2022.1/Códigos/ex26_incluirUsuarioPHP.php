57a6fdd53b0d9f86cc1dcab0dcf90b084d1ab4bb
MatheusJovem <90429176+MatheusJovem@users.noreply.github.com>
2022-06-09 10:54:34 -0300

    Add files via upload

--- 3DAW 2022.1/Códigos/ex26_incluirUsuarioPHP.php
+++ 3DAW 2022.1/Códigos/ex26_incluirUsuarioPHP.php
@@ -1,3 +1,15 @@
 <?php
-    echo "👍";
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $matricula = $_POST["matricula"];
+        $nome = $_POST["nome"];
+        $funcao = $_POST["funcao"];
+        $senha = $_POST["senha"];
+
+        $user = 'root';
+        $pass = '';
+
+        $conn = new PDO('mysql:host=localhost;dbname=3dawj', $user, $pass);
+	    $sql = "INSERT INTO `Usuario`(`nome`,`matricula`,`funcao`,`senha`) VALUES ('$nome','$matricula','$funcao','$senha')";
+    }
+    echo $matricula.$nome.$funcao.$senha."👍";
 ?>
